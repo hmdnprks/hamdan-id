d2df400cc8f08f37700169b14ef44dd6359de84a
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2025-05-28 00:49:52 +0700

    feat(sitemap): add sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hamdan.id',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 5000,
+};
